5d884484f8950fb3897f4ba1cf4d52ccbc38e130
eru123 <39824083+eru123@users.noreply.github.com>
2022-11-25 16:17:40 +0800

    Update fix

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const { isUpper, strToUpper, removePrefix } = require('./src/String');
+const { strToUpper } = require('./src/String');
 const core = require('@actions/core');
 const fs = require('fs');
 
 const file = core.getInput('file');
 const prefix = strToUpper(core.getInput('prefix'));
-const env = Object.keys(process.env)
-    .filter(key => key.startsWith(prefix) && isUpper(key))
-    .map(key => `${removePrefix(key, prefix)}=${process.env[key]}`)
-    .join('\n');
+
+const env = core.getInput('env');
 
 fs.writeFile(file, env, err => {
     if (err) {
