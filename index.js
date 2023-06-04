8556a4ffbbded4d1a77924d1b98fe1db789574c1
Jericho Aquino <yeoligoakino@gmail.com>
2023-06-04 21:50:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { isUpper, strToUpper, removePrefix } = require('./src/String');
+const { removePrefix } = require('./src/String');
 const core = require('@actions/core');
 const fs = require('fs');
 const path = require('path');
 
 const file = core.getInput('file');
-const prefix = strToUpper(core.getInput('prefix'));
+const prefix = core.getInput('prefix');
 const env = Object.keys(process.env)
-    .filter(key => key.startsWith(prefix) && isUpper(key))
+    .filter(key => key.startsWith(prefix))
     .map(key => `${removePrefix(key, prefix)}="${process.env[key]}"`)
     .join('\n');
 
